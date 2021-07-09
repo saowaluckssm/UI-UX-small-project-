45f85d89ed23bea908607cb82e4eca633dc0d066
Saowaluck Morales <71614464+saowaluckssm@users.noreply.github.com>
2021-07-09 16:25:13 -0400

    Popup

---
+++ Popup/script.js
@@ -0,0 +1,12 @@
+const container = document.getElementById("container");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  console.log("click");
+  container.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  container.classList.remove("active");
+});
