59d80ce2d9a50d90a8961c8576bff1a5c560ba7b
Saowaluck Morales <71614464+saowaluckssm@users.noreply.github.com>
2021-07-11 19:48:32 -0400

    Falling Star

---
+++ Falling Star/script.js
@@ -0,0 +1,16 @@
+const createStar = () => {
+  const star = document.createElement("div");
+  star.classList.add("star");
+  star.style.left = Math.random() * 100 + "vw";
+
+  star.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+  star.innerText = "✶";
+  document.body.appendChild(star);
+
+  setTimeout(() => {
+    star.remove();
+  }, 5000);
+};
+
+setInterval(createStar, 1000);
